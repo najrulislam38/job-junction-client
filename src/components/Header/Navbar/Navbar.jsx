6cbcd29b2fc952b774c454761e15318bbc0c7e54
Md Najrul Islam <mdnajrulislam.4738@gmail.com>
2023-11-05 23:34:42 +0600

    added form and make design in the add job page.

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -63,76 +63,82 @@ const Navbar = () => {
   };
 
   return (
-    <div className="max-w-screen-xl mx-auto px-5 md:px-10 py-4">
-      <div className="w-full flex justify-between items-center  bg-base-100">
-        <div className="flex items-center ">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="navLinks flex flex-col gap-2 dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              {navLinks}
-            </ul>
-          </div>
-          <div className="flex gap-1 items-end">
-            <img src={logo} alt="" className="w-8 lg:w-10" />
-            <h2 className="text-base md:text-xl font-bold">
-              <span className="text-xl md:text-2xl  text-[#008FD4] ">Job.</span>
-              Junction
-            </h2>
-          </div>
-        </div>
-        <div className=" hidden lg:flex">
-          <ul className="navLinks flex  items-center gap-6">{navLinks}</ul>
-        </div>
-        {user ? (
-          <div className="flex gap-4 items-center">
-            <h3 className="font-medium hidden lg:block">{user?.displayName}</h3>
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <img src={user?.photoURL ? user.photoURL : userImg} />
-                </div>
+    <div className="navBar">
+      <div className="max-w-screen-xl mx-auto px-5 md:px-10 py-4">
+        <div className="w-full flex justify-between items-center  bg-base-100">
+          <div className="flex items-center ">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
               </label>
               <ul
                 tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                className="navLinks flex flex-col gap-2 dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <h3 className="font-medium py-2 text-center lg:hidden">
-                  {user?.displayName}
-                </h3>
-                <button
-                  onClick={handleSignOut}
-                  className="bg-[#008FD4] hover:bg-[#0870A1] btn-sm text-white rounded-md duration-300"
-                >
-                  Sign Out
-                </button>
+                {navLinks}
               </ul>
             </div>
+            <div className="flex gap-1 items-end">
+              <img src={logo} alt="" className="w-8 lg:w-10" />
+              <h2 className="text-base md:text-xl font-bold">
+                <span className="text-xl md:text-2xl  text-[#008FD4] ">
+                  Job.
+                </span>
+                Junction
+              </h2>
+            </div>
           </div>
-        ) : (
-          <Link to={"signIn"} className="">
-            <button className="bg-[#008FD4] hover:bg-[#0870A1] text-white py-2 px-5 rounded-md duration-300">
-              Sign In
-            </button>
-          </Link>
-        )}
+          <div className=" hidden lg:flex">
+            <ul className="navLinks flex  items-center gap-6">{navLinks}</ul>
+          </div>
+          {user ? (
+            <div className="flex gap-4 items-center">
+              <h3 className="font-medium hidden lg:block">
+                {user?.displayName}
+              </h3>
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                    <img src={user?.photoURL ? user.photoURL : userImg} />
+                  </div>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                >
+                  <h3 className="font-medium py-2 text-center lg:hidden">
+                    {user?.displayName}
+                  </h3>
+                  <button
+                    onClick={handleSignOut}
+                    className="bg-[#008FD4] hover:bg-[#0870A1] btn-sm text-white rounded-md duration-300"
+                  >
+                    Sign Out
+                  </button>
+                </ul>
+              </div>
+            </div>
+          ) : (
+            <Link to={"signIn"} className="">
+              <button className="bg-[#008FD4] hover:bg-[#0870A1] text-white py-2 px-5 rounded-md duration-300">
+                Sign In
+              </button>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/pages/AddJobs/AddJobs.jsx
+++ src/pages/AddJobs/AddJobs.jsx
@@ -1,10 +1,160 @@
+import useAuth from "../../hooks/useAuth";
 
 const AddJobs = () => {
-    return (
-        <div>
-            <h1>add jobs</h1>
-        </div>
-    );
+  const { user } = useAuth();
+
+  const handleAddJob = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.title.value;
+    const deadline = form.deadline.value;
+    const email = user?.email;
+    const category = form.category.value;
+    const minPrice = form.minPrice.value;
+    const maxPrice = form.maxPrice.value;
+    const desc = form.description.value;
+
+    console.log(title, deadline, email, category, minPrice, maxPrice, desc);
+  };
+
+  return (
+    <div>
+      <div className="max-w-screen-xl mx-auto px-5 md:px-10 my-8 md:my-14 lg:my-20">
+        <h1 className="text-center text-xl md:text-3xl font-semibold mb-8">
+          Add Job Here
+        </h1>
+        <form onSubmit={handleAddJob}>
+          <div className="grid gap-6 mb-6 md:grid-cols-2">
+            <div>
+              <label
+                htmlFor="title"
+                className="block mb-2 font-medium text-gray-900 "
+              >
+                Job Title
+              </label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                placeholder="Job Title"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="deadline"
+                className="block mb-2 font-medium text-gray-900 "
+              >
+                Deadline
+              </label>
+              <input
+                type="datetime-local"
+                id="deadline"
+                name="deadline"
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                placeholder="Doe"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 font-medium text-gray-900 "
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                defaultValue={user?.email}
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                readOnly
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="category"
+                className="block mb-2 font-medium text-gray-900 "
+              >
+                Job Category
+              </label>
+
+              <select
+                name="category"
+                id="category"
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                required
+              >
+                <option value="">Select a category</option>
+                <option value="Web Development">Web Development</option>
+                <option value="Digital Marketing">Digital Marketing</option>
+                <option value="Graphic Design">Graphic Design</option>
+              </select>
+            </div>
+            <div>
+              <label
+                htmlFor="min-price"
+                className="block mb-2 font-medium text-gray-900 "
+              >
+                Minimum Price
+              </label>
+              <input
+                type="number"
+                name="minPrice"
+                id="min-price"
+                placeholder="Minimum price"
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="max-price"
+                className="block mb-2 font-medium text-gray-900 "
+              >
+                Maximum Price
+              </label>
+              <input
+                type="number"
+                name="maxPrice"
+                id="max-price"
+                placeholder="maximum price"
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="description"
+                className="block mb-2 font-medium text-gray-900 "
+              >
+                Description
+              </label>
+              <textarea
+                type="number"
+                name="description"
+                rows={4}
+                id="description"
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                required
+                placeholder="Enter Description"
+              ></textarea>
+            </div>
+          </div>
+
+          <div className="text-center">
+            <button
+              type="submit"
+              className="text-white bg-[#008FD4] hover:bg-[#0870A1] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg w-full sm:w-auto px-5 py-2.5 text-center "
+            >
+              Add Job
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddJobs;
